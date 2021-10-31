804f7b1ac9551238a2548d1b45f1110c9c55d64b
plaul <lmor@kea.dk>
2021-10-31 16:32:17 +0100

    ready for tutorial

---
+++ index.js
@@ -0,0 +1,29 @@
+import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+
+import {
+  renderText,
+  adjustForMissingHash, loadTemplateFromDom,
+} from "./utils.js"
+
+window.addEventListener("load", () => {
+
+  const router = new Navigo("/", { hash: true });
+  adjustForMissingHash()
+  router
+    .on({
+      "/": () => renderText("Home", "content"),
+      "/about": () => renderText("About", "content"),
+      "/products": (match) => {
+        renderText("<h1>TODO</h1>", "content")
+      },
+      "/product/:id": (match) => {
+        renderText(`Product ID: ${match.data.id}`, "content")
+      },
+      "/showMatch": (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content")
+    })
+    .notFound(() => renderText("No page for this route found", "content"))
+    .resolve()
+});
+
+
+window.onerror = (e) => alert(e)

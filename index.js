5b99d2457784a276b5fef7458fa1a09142693098
plaul <lmor@kea.dk>
2021-11-05 17:31:50 +0100

    startcode

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
 
 import {
-  renderText,
-  adjustForMissingHash, loadTemplateFromDom,
+  renderText, adjustForMissingHash, loadTemplate, renderTemplate,
 } from "./utils.js"
 
 window.addEventListener("load", async () => {
-
+  const templateHome = await loadTemplate("./pages/home/home.html")
   const router = new Navigo("/", { hash: true });
   adjustForMissingHash()
   router
     .on({
-      "/": () => renderText("Home", "content"),
-      "/about": () => renderText("About", "content"),
-      "/products": (match) => {
-        renderText("<h1>TODO</h1>", "content")
-      },
-      "/joke": () => renderText("Joke","content"),
-      "/product/:id": (match) => {
-        renderText(`Product ID: ${match.data.id}`, "content")
-      },
-      "/showMatch": (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content")
+      "/": () => renderTemplate(templateHome, "content"),
     })
     .notFound(() => renderText("No page for this route found", "content"))
     .resolve()
--- pages/joke/joke.js
+++
@@ -1,3 +0,0 @@
-const URL = "https://api.chucknorris.io/jokes/random"
-
-//TODO

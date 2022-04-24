141c9fce0b430c7ca37767a1f3f0ae695cdf0d01
plaul <lmor@kea.dk>
2022-04-24 10:41:12 +0200

    Now uses Bootstrap Navbar

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ import "https://unpkg.com/navigo"  //Will create the global Navigo object used b
 
 import {
   renderText,
+  setActiveLink,
   adjustForMissingHash, loadTemplateFromDom,
 } from "./utils.js"
 
 window.addEventListener("load", async () => {
 
   const router = new Navigo("/", { hash: true });
-  adjustForMissingHash()
   router
+    .hooks({
+      before(done, match) {
+        setActiveLink("menu", match.url)
+        done()
+      }
+    })
     .on({
       "/": () => renderText("Home", "content"),
-      "/about": () => renderText("About", "content"),
+      "/about": () => renderText(`
+      <p>See <a href="https://getbootstrap.com/docs/5.0/components/navbar/" target="_blank">here</a> for details about the responsive Bootstrap navbar used in this example</p>
+      `, "content"),
       "/products": (match) => {
         renderText("<h1>TODO</h1>", "content")
       },
-      "/joke": () => renderText("Joke","content"),
+      "/joke": () => renderText("Joke", "content"),
       "/product/:id": (match) => {
         renderText(`Product ID: ${match.data.id}`, "content")
       },
-      "/showMatch": (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content")
+      "/show-match": (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content")
     })
     .notFound(() => renderText("No page for this route found", "content"))
     .resolve()

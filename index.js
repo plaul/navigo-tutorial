c929b1fcec9e29c748a3cbfc0dc6eec547dd65de
plaul <lmor@kea.dk>
2022-04-27 08:18:23 +0200

    minor change

--- index.js
+++ index.js
@@ -16,20 +16,15 @@ window.addEventListener("load", async () => {
         done()
       }
     })
-    .on({
-      "/": () => renderText("Home", "content"),
-      "/about": () => renderText(`
-      <p>See <a href="https://getbootstrap.com/docs/5.0/components/navbar/" target="_blank">here</a> for details about the responsive Bootstrap navbar used in this example</p>
-      `, "content"),
-      "/products": (match) => {
-        renderText("<h1>TODO</h1>", "content")
-      },
-      "/joke": () => renderText("Joke", "content"),
-      "/product/:id": (match) => {
-        renderText(`Product ID: ${match.data.id}`, "content")
-      },
-      "/show-match": (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content")
+    .on("/", () => renderText("Home", "content"))
+    .on("/about", () => renderText(`
+    <p>See <a href="https://getbootstrap.com/docs/5.0/components/navbar/" target="_blank">here</a> for details about the responsive Bootstrap navbar used in this example</p>
+    `, "content"))
+    .on( "/products", (match) => {
+      renderText("<h1>TODO</h1>", "content")
     })
+    .on("/joke", () => renderText("Joke", "content"),)
+    .on( "/show-match", (match) => renderText(`<pre>${JSON.stringify(match, null, 2)}</pre>`, "content"))
     .notFound(() => renderText("No page for this route found", "content"))
     .resolve()
 });
